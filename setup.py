9df6bb363df5d17f75391c5ded286ea3bfdf0648
Wesley Hansen <wes@ridersdiscount.com>
2012-06-04 15:51:10 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python
-#encoding: utf-8
 
 from distutils.core import setup
 
 setup(
-	name='ConfigTreeView',
-	version='0.1.0',
+	name='configtreeview',
+	version='0.1.2',
 	author='Wesley Hansen',
 	author_email='wes@ridersdiscount.com',
 	packages=['configtreeview', 'configtreeview.tools'],
-	scripts=['bin/config_example.py', 'bin/custom_treeview.py'],
 	url='http://github.com/wrhansen/ConfigTreeView',
-	license='LICENSE.txt',
+	license='GPLv3',
 	description='An easily configurable GtkTreeView implementation for pygtk.',
 	long_description = open('README.txt').read(),
-	install_requires = ["PyGTK >= 2.0.0", "simplejson"],
+	platforms=["POSIX", "Windows"],
+	requires=["pygtk (>=2.0.0)", "simplejson"],
 )
