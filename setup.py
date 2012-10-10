8e60d851c6d9f35db3115a47cd58bf9878c9b0c9
Wes Hansen <wes@HOL-SRV-APP02.(none)>
2012-10-10 13:46:52 -0400

    Updated setup to reflect new license

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 
 from setuptools import setup, find_packages
 
-required = ['simplejson']
 try:
 	import pygtk
 	pygtk.require('2.0')
@@ -11,14 +10,20 @@ except (ImportError,AssertionError) as e:
 	print "Adding 'pygtk >= 2.0.0' to install_requires list"
 	required.append( 'pygtk >= 2.0.0' )
 
+try:
+	import simplejson
+except ImportError:
+	print "Adding simplejson to install_requires list"
+	required.append('simplejson')
+
 setup(
 	name='configtreeview',
-	version='0.1.31',
+	version='0.1.32',
 	author='Wesley Hansen',
 	author_email='wes@ridersdiscount.com',
 	packages=find_packages(),
 	url='http://github.com/wrhansen/ConfigTreeView',
-	license='GPLv3',
+	license='MIT',
 	description='An easily configurable GtkTreeView implementation for pygtk.',
 	long_description = open('README.rst').read(),
 	platforms=["POSIX", "Windows"],
